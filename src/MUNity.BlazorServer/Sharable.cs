8d00edbda2b88900c53b5465ffa9c18dc5802cd9
Peer Conradi <peerconradi@gmail.com>
2023-04-23 16:02:55 +0200

    Allows Managing of topics

---
+++ src/MUNity.BlazorServer/Sharable.cs
@@ -0,0 +1,13 @@
+﻿using Radzen;
+
+namespace MUNity.BlazorServer
+{
+    public static class Sharable
+    {
+        public static readonly ConfirmOptions YesNoOptions = new ConfirmOptions()
+        {
+            OkButtonText = "Ja",
+            CancelButtonText = "Nein"
+        };
+    }
+}
